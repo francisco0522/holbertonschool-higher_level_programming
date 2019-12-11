78e135d49afe7dcee78cacb301119a10b80c84a9
Francisco <pacho0522@gmail.com>
2019-12-11 00:28:28 +0000

    Adding task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-upper = 0
+up = 0
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(i - upper)), end="")
-        upper = 32 if upper == 0 else 0
+    print("{}".format(chr(i - up)), end="")
+    up = 32 if up == 0 else 0
