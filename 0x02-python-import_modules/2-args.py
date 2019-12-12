a1e310819599efbe4486876e9f34faf61a7846cd
Francisco <pacho0522@gmail.com>
2019-12-12 19:34:29 +0000

    Adding task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-arguments = len(sys.argv) - 1
-print("{} arguments".format(arguments))
-for i in range(1, arguments + 1):
+print("{} arguments".format(len(sys.argv) - 1))
+for i in range(1, len(sys.argv)):
     argum = sys.argv[i]
     print ("{}: {}".format(i,argum))
