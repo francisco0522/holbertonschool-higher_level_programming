ee7f05e0b0759d9b4149154e5d7df8b1bb939ad3
Francisco <pacho0522@gmail.com>
2019-12-12 21:43:59 +0000

    Adding task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
@@ -22,4 +22,4 @@ if __name__ == "__main__":
             print("{} / {} = {}".format(a, b, c))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit (1)
+            exit(1)
