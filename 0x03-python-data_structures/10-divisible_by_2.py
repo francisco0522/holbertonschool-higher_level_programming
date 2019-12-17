a2ddeab1f84a41fcfebef4ae77b9860d17fc6a95
Francisco <pacho0522@gmail.com>
2019-12-17 22:19:46 +0000

    Adding task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list = []
+    for i in my_list:
+        if i % 2 == 0:
+            list.apend(True)
+        else:
+            list.apend(False)
+    return list
