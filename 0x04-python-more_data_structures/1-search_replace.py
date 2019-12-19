82bc9d3c5c651c03089ee2de23b0c35e2da057d4
Francisco <pacho0522@gmail.com>
2019-12-19 18:31:28 +0000

    Adding task 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = my_list
-    ind = new.index(search)
-    del new[ind]
-    new.insert(ind, replace) 
-    return new
+    return [replace if x == search else x for x in new]
