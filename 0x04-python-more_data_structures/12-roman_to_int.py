0dd315e17c510747067a112d42ac6ac0b39fd1a2
Francisco <pacho0522@gmail.com>
2019-12-19 22:40:23 +0000

    Addin task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string:
+        num = [1, 5, 10, 50, 100, 500, 1000]
+        roman = ["I", "V", "X", "L", "C", "D", "M"]
+        result = 0
+        for i in range(len(roman_string)):
+            idx = roman.index(roman_string[i])
+            result = result + num[idx]
+        return result
+        #return [replace if x == search else x for x in new]
+    else:
+        return 0
