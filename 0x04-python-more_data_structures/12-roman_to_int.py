c6aba6ca7af4e2883c63f8712c69066ae6afc4ce
Francisco <pacho0522@gmail.com>
2019-12-19 22:50:14 +0000

    Adding task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,15 @@ def roman_to_int(roman_string):
         num = [1, 5, 10, 50, 100, 500, 1000]
         roman = ["I", "V", "X", "L", "C", "D", "M"]
         result = 0
+        prev = 1001
         for i in range(len(roman_string)):
             idx = roman.index(roman_string[i])
-            result = result + num[idx]
+            if prev >= num[idx]:
+                result = result + num[idx]
+                prev = num[idx]
+            else:
+                result = num[idx] - result
+                prev = num[idx]
         return result
     else:
         return 0
