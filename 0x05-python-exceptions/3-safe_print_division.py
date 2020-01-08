e6056f52f392459822698f0a18488315cc471f30
Pacho0522 <pacho0522@gmail.com>
2020-01-08 18:20:51 -0500

    Adding task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    c = 0
+    try:
+        c = a / b
+        print("Inside result: {}".format(c))
+        return c
+    except ZeroDivisionError:
+        print("Inside result: None")
+        return ("None")
