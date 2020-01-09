9697c21ddd1c0848fbbae18ef29c9819dfc4a99a
Pacho0522 <pacho0522@gmail.com>
2020-01-09 18:25:56 -0500

    fixing task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,12 +4,15 @@
 
 class Square:
     """Square with a private instance size"""
+    
     def __init__(self, size=0):
         self.size = size
-
+    
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """Square with a private instance size"""
+     def __init__(self, size=0):
+        self.size = size
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+             self.__size = value
+
+    def area(self):
+        self.area = self.__size * self.__size
+        return self.area
