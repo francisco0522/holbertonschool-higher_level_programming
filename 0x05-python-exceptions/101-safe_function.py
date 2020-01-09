abc2a665835dc67bb17f4b18f381bddb65009e6e
Pacho0522 <pacho0522@gmail.com>
2020-01-08 23:34:35 -0500

    Adding task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        func = fct(*args)
+    except Exception as ex:
+        print("Exception: {}".format(ex),file=sys.stderr)
+        return None
+    return func
