c7ea51297c69ce480feb4fa463beeed1be2eb102
Pacho0522 <pacho0522@gmail.com>
2020-01-09 19:24:52 -0500

    Adding task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """Square with a private instance size"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def position(self, value):
+        i = value[0]
+        j = value[1]
+        if i > j and i > 0 and j > 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
