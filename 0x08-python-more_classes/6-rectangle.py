f4abbefc67d417cae4752b53a6a18597b08b8e50
Pacho0522 <pacho0522@gmail.com>
2020-01-14 18:48:01 -0500

    Adding task 4, 5, 6

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Rectangle:
+    """Class"""
+    number_of_instances = 0
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+    
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        if self.__width == 0 or self.height == 0:
+            return("")
+
+        rec = []
+        for i in range(self.__height):
+            [rec.append("#") for j in range(self.__width)]
+            if i is not self.__height - 1:
+                rec.append("\n")
+        return ("".join(rec))
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print  ("Bye rectangle...")
