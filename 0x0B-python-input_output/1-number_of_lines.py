a68b55e0b65016a90f10049695affeae8cd51b37
Pacho <pacho0522@gmail.com>
2020-01-21 17:06:00 -0500

    Adding task

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
     count = 0
-    for line in open(filename): count += 1
+    for line in open(filename):
+        count += 1
     return (count)
--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     count = 0
-    for line in open(filename): count += 1
+    for line in open(filename):
+        count += 1
     with open(filename) as file:
         if nb_lines <= 0 or nb_lines >= count:
             data = file.read()
@@ -12,5 +13,3 @@ def read_lines(filename="", nb_lines=0):
             print(line)
             line = file.readline()
             i += 1
-
-
