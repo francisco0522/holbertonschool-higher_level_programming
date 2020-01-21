32a4dba23ccbf04a4ef6a3087c035fb9f6de4695
Pacho <pacho0522@gmail.com>
2020-01-21 18:40:17 -0500

    Adding task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            if all(isinstance(i, str) for i in attrs):
+                return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        else:
+            return self.__dict__ 
