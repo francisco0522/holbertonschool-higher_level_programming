30420d675ce31e47c39f668667adc1d759f1636f
Pacho <pacho0522@gmail.com>
2020-01-21 17:13:23 -0500

    Adding task 7

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -4,4 +4,4 @@ import json
 
 def save_to_json_file(my_obj, filename):
     with open(filename, "w") as file:
-            return(file.write(text))
+            return(file.write(json.dumps(my_obj)))
