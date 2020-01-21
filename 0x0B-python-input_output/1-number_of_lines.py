5c8d2d248ed8dd7bc013c7c6c40a4c70757e9141
Pacho <pacho0522@gmail.com>
2020-01-21 17:18:45 -0500

    Adding fix 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
     count = 0
-    for line in open(filename):
-        count += 1
-    return (count)
+    with open(filename) as file:
+        for line in file:
+            count += 1
+        return (count)
