293e61d973e2c4eb592ae79291457c99afdcfe32
root <root@LAPTOP-R4SBV98G.localdomain>
2020-01-24 16:35:01 -0500

    Adding task to 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,3 +63,11 @@ class Rectangle(Base):
     def area(self):
         return self.__width * self.__height
 
+    def display(self):
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print("")
+
+    def __str__(self):
+        return("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height ))
