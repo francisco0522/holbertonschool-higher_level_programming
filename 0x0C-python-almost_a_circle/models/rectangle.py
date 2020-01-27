a680272ea1c6afc115b0c428d6c4ac50ebfa8275
root <root@LAPTOP-R4SBV98G.localdomain>
2020-01-27 14:45:14 -0500

    Fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,8 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """class"""
-    __nb_objects = 0
+    """ rectangle class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Init"""
