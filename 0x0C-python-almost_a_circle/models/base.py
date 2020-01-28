da6b2e1b9c30f89101e3025d3949cf34969931e1
root <root@DESKTOP-GAH54SL.localdomain>
2020-01-27 21:41:52 -0500

    Fixing task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -59,4 +59,3 @@ class Base:
             return []
         with open(cls.__name__ + ".json", "r", encoding="utf-8") as f:
             return [cls.create(**d) for d in cls.from_json_string(f.read())]
-
