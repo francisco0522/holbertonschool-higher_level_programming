790170d2bf473f857db4d78a5ac55611466d633f
francisco0522 <pacho0522@gmail.com>
2020-03-29 02:19:24 -0500

    Adding task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""lists all State objects from the database"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    data = session.query(State).order_by(State.id).filter(State.name.like('%a%'))
+    for state in data:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
