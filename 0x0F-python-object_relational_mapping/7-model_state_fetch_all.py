0256e26df2852721e67f915b39228151b26248af
francisco0522 <pacho0522@gmail.com>
2020-03-29 01:44:25 -0500

    Fixing task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,11 +10,12 @@ if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-    .format(user, passwd, db), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(user, passwd, db), pool_pre_ping=True)
 
 Session = sessionmaker(bind=engine)
 session = Session()
-for state in session.query(State).order_by(State.id).all(): 
+for state in session.query(State).order_by(State.id).all():
     print("{}: {}".format(state.id, state.name))
 session.close()
