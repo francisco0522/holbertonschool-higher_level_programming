1061fff0409e0c05f4ee5bfc0fb0c66a4d2e4d13
francisco0522 <pacho0522@gmail.com>
2020-03-29 02:00:25 -0500

    Fixing task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
             sys.argv[3]),
             pool_pre_ping=True
         )
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-records = session.query(State).order_by(State.id).all()
-for row in records:
-    print("{}: {}".format(row.id, row.name))
-session.close()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    records = session.query(State).order_by(State.id).all()
+    for row in records:
+        print("{}: {}".format(row.id, row.name))
+    session.close()
