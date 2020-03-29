81ca9f4945d0f8425462750ea796d36cd92520f0
francisco0522 <pacho0522@gmail.com>
2020-03-29 02:38:45 -0500

    Adding task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    name = argv[4]
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(user, passwd, db), pool_pre_ping=True)
