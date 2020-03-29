f23e4a816e271f9f77604dd543d26cdd44721c04
francisco0522 <pacho0522@gmail.com>
2020-03-29 02:30:15 -0500

    Fixing task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,10 +18,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    d = session.query(State).filter_by(name=name).all()
-    for state in d:
-        if not state:
-            print("Not found")
-        else:
-            print("{}".format(state.id))
+    state = session.query(State).filter_by(name=name).all()
+    if not state:
+        print("Not found")
+    for cases in state:
+        print("{}".format(cases.id))
     session.close()
