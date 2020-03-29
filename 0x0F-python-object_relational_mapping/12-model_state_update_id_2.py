81621263a8ecf40b8a554eb3722307996c6e9b72
francisco0522 <pacho0522@gmail.com>
2020-03-29 02:57:21 -0500

    Adding task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).filter_by(id=2)
-    print("{}".format(state))
+    print("{}".format(state.id))
     state.name = 'New Mexico'
     session.commit()
     session.close()
