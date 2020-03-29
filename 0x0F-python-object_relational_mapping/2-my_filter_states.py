34c754b6c9aaf6d38b6e378295aaec67aee3ace8
francisco0522 <pacho0522@gmail.com>
2020-03-28 22:37:00 -0500

    Fixing task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                                 passwd=passwd, db=db, port=3306)
     cursor = connectDb.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY states.id"
+        "SELECT * FROM states WHERE BINARY states.name = '{}' ORDER BY states.id"
         .format(arg))
     rows = cursor.fetchall()
     for i in rows:
