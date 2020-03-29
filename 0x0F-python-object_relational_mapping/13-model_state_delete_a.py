0bb8c9a1669bbca29cbf80d894d94e308f6ffbb6
francisco0522 <pacho0522@gmail.com>
2020-03-29 03:06:20 -0500

    Adding task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    d = session.query(State).filter(State.name.like('%a%')).delete()
+    d = session.query(State).filter(State.name.like('%a%')).\
+        delete(synchronize_session='fetch')
     session.commit()
     session.close()
