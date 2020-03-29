442ec85262bf2b6b96b107ba5b9b0c4700434998
francisco0522 <pacho0522@gmail.com>
2020-03-29 02:25:45 -0500

    Adding task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name.like (name))
+    state = session.query(State).filter(State.name.like(name))
     if not state:
         print("Not found")
     else:
