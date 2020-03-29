b4a05fe6abda350a5062949c2770843c2607b7a9
francisco0522 <pacho0522@gmail.com>
2020-03-29 02:39:59 -0500

    Adding task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    louisiana = State("Louisiana")
+    louisiana = State(6, "Louisiana")
     session.add(louisiana)
     session.commit()
     session.close()
