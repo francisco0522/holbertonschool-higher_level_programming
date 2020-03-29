bf184ff5935761667076da3dc763bff659986d65
francisco0522 <pacho0522@gmail.com>
2020-03-29 01:56:50 -0500

    Fixing task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """lists all State objects from the database"""
-from sys import argv
+import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
