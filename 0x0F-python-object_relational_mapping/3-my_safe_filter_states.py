416d24d16c6a3ea342867bc5d3b49523dddce9ec
francisco0522 <pacho0522@gmail.com>
2020-03-28 23:15:53 -0500

    Adding task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    arg = argv[4]
+    connectDb = MySQLdb.connect(host='localhost', user=user,
+                                passwd=passwd, db=db, port=3306)
+    cursor = connectDb.cursor()
+    cursor.execute("""SELECT * FROM states
+        WHERE BINARY states.name = %s ORDER BY states.id""".format(arg))
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+    cursor.close()
+    connectDb.close()
