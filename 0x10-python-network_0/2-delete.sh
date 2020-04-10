94e73d997e179220ff4002ec0e79297aca93dd16
francisco0522 <pacho0522@gmail.com>
2020-04-09 19:04:10 -0500

    Fixing files

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # sends a DELETE request to the URL 
-# passed as the first argument and 
-# displays the body of the response
 curl -s -XDELETE "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# takes in a URL 
-# and displays all HTTP methods 
-# the server will accept.
+# and displays all HTTP methods
 curl -sI "$1" | grep "Allow" | cut -d " " -f2-
