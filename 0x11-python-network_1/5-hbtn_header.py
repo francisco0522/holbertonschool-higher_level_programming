d77e486be4e8b39f50b08e870691e2fb50a77e88
francisco0522 <pacho0522@gmail.com>
2020-04-15 22:25:52 -0500

    Adding task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Response header value
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
