2ee9ea9f2b0a71fd191b38b887e17e8d67669d93
francisco0522 <pacho0522@gmail.com>
2020-04-15 22:31:14 -0500

    Adding task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 
+POST an email
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    data={'email': argv[2]}
+    response = requests.post(argv[1], data)
+    print(response.text)
