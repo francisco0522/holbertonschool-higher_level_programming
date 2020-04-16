95d7d01638ae43c48ef0b427eecc7c5f179d8de3
francisco0522 <pacho0522@gmail.com>
2020-04-15 21:55:42 -0500

    Adding task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,4 +9,4 @@ from sys import argv
 if __name__ == "__main__":
     req = urllib.request.Request(argv[1])
     with urllib.request.urlopen(req) as response:
-      print(response.headers.get('X-Request-Id'))
+        print(response.headers.get('X-Request-Id'))
