058d624746e1f371d2bee5102cca8285238f0f89
francisco0522 <pacho0522@gmail.com>
2020-05-05 20:13:36 -0500

    Adding task 11, 12, 13

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let big = 0;
+let second = 0;
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    if (big < process.argv[i]) {
+      second = big;
+      big = process.argv[i];
+    }
+  }
+  console.log(second);
+}
