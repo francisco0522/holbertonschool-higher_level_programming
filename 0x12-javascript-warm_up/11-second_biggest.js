90720b7bb38f2f74571c9b181831e482a58a097b
francisco0522 <pacho0522@gmail.com>
2020-05-05 20:41:03 -0500

    Fixing 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,15 @@
 let big = 0;
 let second = 0;
 if (process.argv.length <= 3) {
-  console.log('0');
+  console.log(0);
 } else {
   for (let i = 2; i < process.argv.length; i++) {
     if (big < process.argv[i]) {
       second = big;
       big = process.argv[i];
+    }
+    if (big === process.argv[i]) {
+      second = big;
     }
   }
   console.log(second);
