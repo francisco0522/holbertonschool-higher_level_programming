2f28e820ad06f18cb122249e76c94f81eae2aaf6
francisco0522 <pacho0522@gmail.com>
2020-05-05 20:29:00 -0500

    fixing task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 let big = 0;
 let second = 0;
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
   for (let i = 2; i < process.argv.length; i++) {
     if (big < process.argv[i]) {
