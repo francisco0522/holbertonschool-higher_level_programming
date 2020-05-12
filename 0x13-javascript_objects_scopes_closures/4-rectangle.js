fe1a9e14325d713be7017396bcebc0b9209f8428
francisco0522 <pacho0522@gmail.com>
2020-05-11 20:03:36 -0500

    Adding task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+let x = '';
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+      x = '';
+    }
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+
+  rotate () {
+    const temporal = this.width;
+    this.width = this.height;
+    this.height = temporal;
+  }
+}
+module.exports = Rectangle;
