8b2c58d0787b671ba4f419dbabc850fb6a34de52
francisco0522 <pacho0522@gmail.com>
2020-05-11 20:35:23 -0500

    Adding task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+let x = '';
+
+class Square extends require('./5-square') {
+  constructor (s) {
+    super(s, s);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        if (c === undefined) {
+          x += 'X';
+        } else {
+          x += 'C';
+        }
+      }
+      console.log(x);
+      x = '';
+    }
+  }
+}
+
+module.exports = Square;
