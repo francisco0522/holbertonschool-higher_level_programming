87139ca2e1b85ec265a97c301f6634e12f7b0bcb
francisco0522 <pacho0522@gmail.com>
2020-05-12 20:52:14 -0500

    Fixing task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); } else {
+  if (err) { console.log('code:', res.statusCode); } else {
     let x = 0;
     for (const film of body.results) {
       for (const character of film.characters) {
