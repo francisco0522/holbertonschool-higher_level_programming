f697ac38b9115e9a9c96ae1b3ecb48ff8454f694
francisco0522 <pacho0522@gmail.com>
2020-05-12 19:14:05 -0500

    Fixing

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,7 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response) {
-  console.log('code:', response && response.statusCode);
+  if (error) {
+  }
+  console.log('statusCode:', response && response.statusCode);
 });
