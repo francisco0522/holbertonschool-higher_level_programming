88acf79986a11cf471def0cf38e540fdc9bcb6ba
francisco0522 <pacho0522@gmail.com>
2020-05-12 21:39:56 -0500

    Adding task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,26 +2,24 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+request(url, { json: true }, (err, res, body) => {
+  if (err) {console.log(err); } else {
     let task = 0;
     let y = 1;
-    const res = {};
+    const result = {};
     for (let i = 0; i < body.length; i++) {
       if (i > 0) {
         y = body[i - 1].userId;
       }
       if (body[i].userId !== y) {
-        res[y] = task;
+        result[y] = task;
         task = 0;
       }
       if (body[i].completed === true) {
         task = task + 1;
       }
     }
-    res[y] = task;
-    console.log(res);
+    result[y] = task;
+    console.log(result);
   }
 });
